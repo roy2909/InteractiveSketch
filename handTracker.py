82b08b3cb46e681c1878b708d86f7c657eaa6e35
Rahul Roy <rahulroy2909@gmail.com>
2024-05-30 00:15:40 -0500

    added findHand function

--- handTracker.py
+++ handTracker.py
@@ -22,3 +22,23 @@ class HandTracker():
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detection_confidence, self.tracking_confidence)
         self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, img, draw=True):
+        """
+        Detect hands in the provided image and draw landmarks if draw is True.
+        
+        Parameters:
+        - img: The input image.
+        - draw: Whether to draw the hand landmarks on the image.
+        
+        Returns:
+        - The image with or without hand landmarks.
+        """
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+
+        if self.results.multi_hand_landmarks:
+            for handlandmarks in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handlandmarks, self.mpHands.HAND_CONNECTIONS)
+        return img
