310a6034b320351eef6203833f427c09c4cb6067
Rahul Roy <rahulroy2909@gmail.com>
2024-05-30 00:19:47 -0500

    formatting

--- handTracker.py
+++ handTracker.py
@@ -3,11 +3,12 @@ import numpy as np
 import cv2
 import math
 
+
 class HandTracker():
     def __init__(self, mode=False, maxHands=2, detection_confidence=0.5, tracking_confidence=0.5):
         """
         Initialize the HandTracker object.
-        
+
         Parameters:
         - mode: Static image mode or not.
         - maxHands: Maximum number of hands to detect.
@@ -20,17 +21,18 @@ class HandTracker():
         self.tracking_confidence = tracking_confidence
 
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detection_confidence, self.tracking_confidence)
+        self.hands = self.mpHands.Hands(
+            self.mode, self.maxHands, self.detection_confidence, self.tracking_confidence)
         self.mpDraw = mp.solutions.drawing_utils
 
     def findHands(self, img, draw=True):
         """
         Detect hands in the provided image and draw landmarks if draw is True.
-        
+
         Parameters:
         - img: The input image.
         - draw: Whether to draw the hand landmarks on the image.
-        
+
         Returns:
         - The image with or without hand landmarks.
         """
@@ -40,18 +42,19 @@ class HandTracker():
         if self.results.multi_hand_landmarks:
             for handlandmarks in self.results.multi_hand_landmarks:
                 if draw:
-                    self.mpDraw.draw_landmarks(img, handlandmarks, self.mpHands.HAND_CONNECTIONS)
+                    self.mpDraw.draw_landmarks(
+                        img, handlandmarks, self.mpHands.HAND_CONNECTIONS)
         return img
-    
+
     def getPostion(self, img, num=0, draw=True):
         """
         Get the positions of hand landmarks.
-        
+
         Parameters:
         - img: The input image.
         - num: Index of the hand to process (default is 0).
         - draw: Whether to draw the hand landmarks on the image.
-        
+
         Returns:
         - A list of landmark positions (x, y).
         """
@@ -66,14 +69,14 @@ class HandTracker():
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
         return landmarklist
-    
+
     def getUpFingers(self, img):
         """
         Determine which fingers are up.
-        
+
         Parameters:
         - img: The input image.
-        
+
         Returns:
         - A list of booleans indicating which fingers are up.
         """
@@ -81,27 +84,32 @@ class HandTracker():
         self.upfingers = []
         if pos:
             # Thumb
-            self.upfingers.append((pos[4][1] < pos[3][1] and (pos[5][0] - pos[4][0] > 10)))
+            self.upfingers.append(
+                (pos[4][1] < pos[3][1] and (pos[5][0] - pos[4][0] > 10)))
             # Index finger
-            self.upfingers.append((pos[8][1] < pos[7][1] and pos[7][1] < pos[6][1]))
+            self.upfingers.append(
+                (pos[8][1] < pos[7][1] and pos[7][1] < pos[6][1]))
             # Middle finger
-            self.upfingers.append((pos[12][1] < pos[11][1] and pos[11][1] < pos[10][1]))
+            self.upfingers.append(
+                (pos[12][1] < pos[11][1] and pos[11][1] < pos[10][1]))
             # Ring finger
-            self.upfingers.append((pos[16][1] < pos[15][1] and pos[15][1] < pos[14][1]))
+            self.upfingers.append(
+                (pos[16][1] < pos[15][1] and pos[15][1] < pos[14][1]))
             # Pinky finger
-            self.upfingers.append((pos[20][1] < pos[19][1] and pos[19][1] < pos[18][1]))
+            self.upfingers.append(
+                (pos[20][1] < pos[19][1] and pos[19][1] < pos[18][1]))
         return self.upfingers
 
     def get_distance(self, point1_idx, point2_idx, frame, draw=False):
         """
         Calculate the distance between two landmarks.
-        
+
         Parameters:
         - point1_idx: Index of the first landmark.
         - point2_idx: Index of the second landmark.
         - frame: The input image.
         - draw: Whether to draw the distance measurement on the image.
-        
+
         Returns:
         - The distance between the two points.
         """
